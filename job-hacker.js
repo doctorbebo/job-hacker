9bafdb87c7fb51482a83989e5d0f188f7150dd31
doctorbebo <byronrb92@gmail.com>
2020-02-11 19:29:03 -0800

    Finally Got github API called right

--- job-hacker.js
+++ job-hacker.js
@@ -1,7 +1,20 @@
 // require pdf kit and inqiurer
 const pdfKit = require("PDFKit");
-const inquirer = require("inquirer");
+const process = require("process");
+const axios = require('axios');
+const fs = require("fs");
 
-console.log(inquirer);
 
-//
+
+// request gitHub User name from user. Using Process.
+const username = process.argv[2];
+// query link to github
+const queryURL = `https://api.github.com/users/${username}`;
+axios.get(queryURL)
+  .then((response) => {
+    fs.writeFile("user.txt",JSON.stringify(response.data,null,"\t"), (err) =>
+    {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+  })
